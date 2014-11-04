1fd5ff577102daad4baa61ca94a834e20e32b2bf
mokpro <pranav.moktali@gmail.com>
2014-11-04 10:06:50 -0600

    Creating Repo

---
+++ src/main.js
@@ -0,0 +1,11 @@
+/*global famous*/
+// import dependencies
+var Engine = famous.core.Engine;
+var Modifier = famous.core.Modifier;
+var Transform = famous.core.Transform;
+var ImageSurface = famous.surfaces.ImageSurface;
+
+// create the main context
+var mainContext = Engine.createContext();
+
+// your app here
